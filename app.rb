67a1133c046af3f538586ad849c4415091edc3f3
ehawk823 <evan.alexander.hawk@gmail.com>
2015-08-26 13:47:26 -0500

    done

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require './config/environment'
 
 class TestApp < Sinatra::Base
-  #put your route handlers here
+  get '/about' do
+
+  erb :'about'
+  end
+
+  get '/hello' do
+    @name = params[:name]
+  erb  :'hello'
+  end
+
+  get '/' do
+  end
+
+  post '/' do
+  end
+
 end
